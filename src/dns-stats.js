1c7aa37c61dd5fcc091e59268a19ae29ab804ced
nvalkovich <anastasiya.valkovich@mail.ru>
2023-04-14 22:11:32 +0300

    feat: implement dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getDNSStats(domains) {
+  let stats = {};
+  for(let i = 0; i < domains.length; i++) {
+    let splittedDomain = domains[i].split('.').reverse();
+    for(let j = 0; j < splittedDomain.length; j++) {
+      let property = `.${splittedDomain.slice(0, [j + 1]).join('.')}`;
+      if(!stats.hasOwnProperty(property)){
+        stats[property] = 1;
+      } else {
+        stats[property] += 1;
+      }
+    }
+  }
+return stats;
 }
 
 module.exports = {
