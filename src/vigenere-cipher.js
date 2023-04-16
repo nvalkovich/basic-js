9f96b86968d1a47953c95e6e82f80ef0c5e46a22
nvalkovich <anastasiya.valkovich@mail.ru>
2023-04-16 21:40:34 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  #alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  #isDirect = null;
+
+  constructor(isDirect = true) {
+    this.#isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toLowerCase();
+    key = key.toLowerCase()
+    let messageValues = message.split('').map(x => x = this.#alphabet.indexOf(x));
+    let keyValues = key.split('').map(x => x = this.#alphabet.indexOf(x));
+    let shift = 0;
+    let result = [];
+
+    for (let i = 0; i <= message.length; i++) {
+      if (messageValues[i] >= 0) {
+        let messageValue = messageValues[i];
+        let keyValue = keyValues[(i - shift) % keyValues.length];
+        let value = ((messageValue + keyValue) % 26);
+        result.push(this.#alphabet[value]);
+      } else {
+        shift += 1;
+        result.push(message[i])
+      }
+    }
+
+    return this.#isDirect === true || '' ? result.join('').toUpperCase() : result.reverse().join('').toUpperCase();
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+    let messageValues = message.split('').map(x => x = this.#alphabet.indexOf(x));
+    let keyValues = key.split('').map(x => x = this.#alphabet.indexOf(x));
+    let shift = 0;
+    let result = [];
+
+    for (let i = 0; i <= message.length; i++) {
+      if (messageValues[i] >= 0) {
+        let messageValue = messageValues[i];
+        let keyValue = keyValues[(i - shift) % keyValues.length];
+        let value = (messageValue - keyValue) >= 0 ? (messageValue - keyValue) % 26 : (((messageValue - keyValue) % 26) + 26) % 26;
+        result.push(this.#alphabet[value]);
+      } else {
+        shift += 1;
+        result.push(message[i])
+      }
+    }
+
+    return this.#isDirect === true || '' ? result.join('').toUpperCase() : result.reverse().join('').toUpperCase();
   }
 }
 
